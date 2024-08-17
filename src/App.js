db5898199f096c563facbe9b9264ae6bf042f6ce
Senuja Jayasekara <senujajayaekara@gmail.com>
2024-08-17 14:34:46 +0530

    refactored

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './NavBar';
+import NavBar from './Component/NavBar';
 import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1 className='text-3xl'>Hello React!</h1>
-      </header>
     </div>
   );
 }
